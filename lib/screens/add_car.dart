57e0a8c65dbe3d37687b7c84c5efbee36b516d8b
Fabricio Valli <fabriciovalli@gmail.com>
2018-07-28 13:21:44 -0300

    alteracao nos cards de inclusao

--- lib/screens/add_car.dart
+++ lib/screens/add_car.dart
@@ -1,13 +1,59 @@
 import 'package:app4car/models/car_data.dart';
-import 'package:app4car/widgets/add_car_card.dart';
 import 'package:flutter/material.dart';
 
-class AddCarTab extends StatelessWidget {
-  final CarData carData = new CarData(
+class AddCarTab extends StatefulWidget {
+  @override
+  _AddCarTabState createState() => _AddCarTabState();
+}
+
+class _AddCarTabState extends State<AddCarTab> {
+  static final CarData carData = new CarData(
       brand: 'Chevrolet',
       model: 'Onix',
       modelComplement: 'Advantage 1.4',
       year: 2017);
+
+  List<CarAddItem> items = <CarAddItem>[
+    new CarAddItem(
+      car: carData,
+      isExpanded: false,
+      header: 'Schools',
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(children: <Widget>[]),
+      ),
+      icon: Icon(
+        Icons.drive_eta,
+        size: 40.0,
+      ),
+    ),
+    new CarAddItem(
+      car: carData,
+      isExpanded: false,
+      header: 'Schools',
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(children: <Widget>[]),
+      ),
+      icon: Icon(
+        Icons.drive_eta,
+        size: 40.0,
+      ),
+    ),
+    new CarAddItem(
+      car: carData,
+      isExpanded: false,
+      header: 'Schools',
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(children: <Widget>[]),
+      ),
+      icon: Icon(
+        Icons.drive_eta,
+        size: 40.0,
+      ),
+    ),
+  ];
 
   Widget _buildSerchBar(BuildContext context) {
     return TextFormField(
@@ -17,32 +63,60 @@ class AddCarTab extends StatelessWidget {
         hintText: 'Buscar Veículo',
         suffixIcon: new IconButton(
           onPressed: () {},
-          icon: new Icon(Icons.search, color: Theme.of(context).buttonColor,),
+          icon: new Icon(
+            Icons.search,
+            color: Theme.of(context).buttonColor,
+          ),
         ),
       ),
     );
   }
 
   Widget _buildCarList(BuildContext context) {
-    return Expanded(
-      child: ListView(
-        children: <Widget>[
-         AddCarCard(
-            carData: carData,
-          ),
-          AddCarCard(
-            carData: carData,
-          ),
-          AddCarCard(
-            carData: carData,
-          ),
-          AddCarCard(
-            carData: carData,
-          ),
-        ],
-      ),
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        setState(() {
+          items[index].isExpanded = !items[index].isExpanded;
+        });
+      },
+      children: items.map((CarAddItem item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return ListTile(
+              dense: false,
+              leading: item.icon,
+              title: Text(
+                item.car.model,
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14.0),
+              ),
+            );
+          },
+          isExpanded: item.isExpanded,
+          body: item.body,
+        );
+      }).toList(),
     );
   }
+  // Widget _buildCarList(BuildContext context) {
+  //   return Expanded(
+  //     child: ListView(
+  //       children: <Widget>[
+  //        AddCarCard(
+  //           carData: carData,
+  //         ),
+  //         AddCarCard(
+  //           carData: carData,
+  //         ),
+  //         AddCarCard(
+  //           carData: carData,
+  //         ),
+  //         AddCarCard(
+  //           carData: carData,
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -57,4 +131,13 @@ class AddCarTab extends StatelessWidget {
       ),
     );
   }
+}
+
+class CarAddItem {
+  bool isExpanded;
+  final String header;
+  final Widget body;
+  final Icon icon;
+  final CarData car;
+  CarAddItem({this.car, this.isExpanded, this.header, this.body, this.icon});
 }
--- lib/screens/parking/step_one.dart
+++ lib/screens/parking/step_one.dart
@@ -9,10 +9,13 @@ Widget _buildParkedCars(BuildContext context) {
   final double appBarSize = 180.0;
   final double paddingTop = 16.0;
 
-  final Size topCarSize = Size(screenSize.width / 3, (screenSize.height - appBarSize - paddingTop) * 0.4);
+  final Size topCarSize = Size(screenSize.width / 3,
+      (screenSize.height - appBarSize - paddingTop) * 0.4);
   topCarHeight = topCarSize.height;
-  final Size parkingSpotSize = Size(screenSize.width / 3, (screenSize.height - appBarSize - paddingTop) * 0.45);
-  final Size bottomCarSize = Size(screenSize.width / 3, (screenSize.height - appBarSize - paddingTop) * 0.15);
+  final Size parkingSpotSize = Size(screenSize.width / 3,
+      (screenSize.height - appBarSize - paddingTop) * 0.45);
+  final Size bottomCarSize = Size(screenSize.width / 3,
+      (screenSize.height - appBarSize - paddingTop) * 0.15);
 
   return Container(
     width: topCarSize.width,
@@ -23,26 +26,31 @@ Widget _buildParkedCars(BuildContext context) {
         new Container(
           height: topCarSize.height,
           decoration: BoxDecoration(
-              border: Border.all(color: kApp4CarBlueBorder, width: 2.0)
-          ),
+              border: Border.all(color: kApp4CarBlueBorder, width: 2.0)),
           child: Image.asset("assets/car-white.png"),
         ),
-        new SizedBox(height: 8.0,),
+        new SizedBox(
+          height: 8.0,
+        ),
         new Container(
           height: parkingSpotSize.height,
           decoration: BoxDecoration(
               border: Border.all(color: kApp4CarGreen, width: 2.0),
-              borderRadius: BorderRadius.circular(8.0)
-          ),
+              borderRadius: BorderRadius.circular(8.0)),
         ),
-        new SizedBox(height: 8.0,),
-
+        new SizedBox(
+          height: 8.0,
+        ),
         Expanded(
           child: new Container(
             decoration: BoxDecoration(
-                border: Border.all(color: kApp4CarBlueBorder, width: 2.0)
+                border: Border.all(color: kApp4CarBlueBorder, width: 2.0)),
+            child: Image.asset(
+              "assets/car-white.png",
+              width: parkingSpotSize.width,
+              fit: BoxFit.none,
+              alignment: Alignment(0.0, -1.0),
             ),
-            child: Image.asset("assets/car-white.png", width: parkingSpotSize.width, fit: BoxFit.none, alignment: Alignment(0.0, -1.0),),
           ),
         ),
       ],
@@ -54,7 +62,8 @@ Widget _buildParkingCar(BuildContext context) {
   final Size screenSize = MediaQuery.of(context).size;
   final double appBarSize = 180.0;
   final double paddingTop = 16.0;
-  final Size parkingCar = Size(screenSize.width / 3, (screenSize.height - appBarSize - paddingTop) * 0.4);
+  final Size parkingCar = Size(screenSize.width / 3,
+      (screenSize.height - appBarSize - paddingTop) * 0.4);
 
   return Container(
     width: parkingCar.width,
@@ -64,19 +73,20 @@ Widget _buildParkingCar(BuildContext context) {
       children: <Widget>[
         Container(
           height: parkingCar.height,
-          child: Stack(
-              children: <Widget>[
-                Image.asset('assets/car-orange.png',),
-                Positioned(
-                  left: 0.0,
-                  top: 0.0,
-                  child: FractionalTranslation(
-                      translation: const Offset(0.13, 0.7),
-                      child: Image.asset('assets/wheel-right.png',)
-                  ),
-                ),
-              ]
-          ),
+          child: Stack(children: <Widget>[
+            Image.asset(
+              'assets/car-orange.png',
+            ),
+            Positioned(
+              left: 0.0,
+              top: 0.0,
+              child: FractionalTranslation(
+                  translation: const Offset(0.13, 0.7),
+                  child: Image.asset(
+                    'assets/wheel-right.png',
+                  )),
+            ),
+          ]),
         ),
       ],
     ),
